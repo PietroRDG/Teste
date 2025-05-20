8aaf3fd3b3719f330616e05fc1b10a2c75879831
PietroRDG <pietro.xet@gmail.com>
2025-05-20 20:48:44 -0300

    adicionando segundo desafio

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Digite o numero: ");
+        int numeroDigitado = scanner.nextInt();
+
+        scanner.close();
+
+        int primeiro = 0, segundo = 1;
+        ArrayList<Integer> listaFibonacci = new ArrayList<>();
+
+        for (int i = 0; i < numeroDigitado; i++) {
+            System.out.print(primeiro + " ");
+            listaFibonacci.add(primeiro);
+
+            int proximo = primeiro + segundo;
+            primeiro = segundo;
+            segundo = proximo;
+        }
+
+        if (listaFibonacci.contains(numeroDigitado)){
+            System.out.println("o numero digitado pertence a sequencia de Fibonacci");
+        } else {
+            System.out.println("o numero não pertence a sequencia de Fibonacci");
         }
     }
 }
